badb8d2adc8e75845d1d9b04ebc784a8a5580d1f
srivathsa.ms <srivathsa.ms@outlook.com>
2013-07-02 00:08:55 +0530

    bst operations committed

--- cplusplus-repo/readme.txt
+++ cplusplus-repo/readme.txt
@@ -1,1 +1,24 @@
 compilation of questions on c++
+
+Goldman Sachs:
+
+1. Explain virtual functions
+	--- vptr
+2. Explain static storage class in conjunction with
+	--- data
+	--- member functions
+	--- instance/example where static member functions are used
+	--- instance/example where static classes might come in handy
+
+3. Explain const keyword usage in terms of 
+	--- data
+	--- member functions
+	--- kept pressing for what const member functions are used for
+
+4. Explain auto_ptr
+
+5. Explain shared_ptr
+
+6. Implement a stack
+
+7. Difference between a pointer and an alias/reference
--- ds-algorithms-repo/BST/BST/BST.cpp
+++ ds-algorithms-repo/BST/BST/BST.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-#include <exception>
+#include <list>
 
 template <class T>
 class Comparable {
@@ -41,6 +41,19 @@ struct TreeNode {
 };
 template <class Key>
 class BSTSymbolTable {
+public:  
+  class BSTRecord {
+  public:
+    bool isValid(){
+      return _key ? true : false;
+    }
+  public:
+    BSTRecord( ):_key (NULL), _pNext(NULL)
+    {
+    }
+    Key* _key;
+    BSTRecord* _pNext;
+  };
 public:
   BSTSymbolTable():
       _pRoot(NULL)
@@ -91,41 +104,245 @@ public:
     }
     return false;
   }
- /* bool get ( Key& key)
+  BSTRecord get (Key& key)
   {
-    if (exists(key)){
-      TreeNode* res = get(_pRoot,key);
-    }
-  }*/
-
-public:
-  void remove (Key& key)
-  {
-
+       TreeNode<Key>* resNode = get(_pRoot,key);
+       BSTRecord record;
+       if (resNode != NULL){
+         record._key = &resNode->_key;
+       }
+       return record;
   }
 
 private:
-  TreeNode* floor (TreeNode* node, Key& key)
+  TreeNode<Key>* inorder_successor(TreeNode<Key>* node, Key& key) {
+    if (node == NULL)
+      return NULL;
+    int nRet = node->_key.compare(key);
+    TreeNode<Key>* retNode = NULL;
+    if (0 == nRet){ /*TreeNode Match Found*/ 
+      //Now find inorder successor
+      retNode = node->_pRight;
+      while ( (retNode != NULL) && (retNode->_pLeft != NULL)){
+        retNode = retNode->_pLeft;
+      }
+
+    }else if (nRet < 0){ //node.val > key => key must be in left sub tree
+      retNode = inorder_successor(node->_pLeft,  key);
+      if (retNode == NULL)
+        retNode = node;
+    }else { //node.val < key => key must be in right sub tree
+      retNode = inorder_successor(node->_pRight,  key);
+      // if inorder successor of right sub tree is null
+      // then no such element exists
+    }
+
+    return retNode;
+  }
+
+  TreeNode<Key>* inorder_successor(Key& key) {
+    TreeNode<Key>* nextNode = inorder_successor(_pRoot, key);
+    return nextNode;
+  }
+
+  //TreeNode<Key>* remove (TreeNode<Key>* node, Key& key)
+  //{
+  //  if (node == NULL)
+  //    return NULL;
+  //  int nRet = node->_key.compare(key);
+  //  
+  //  TreeNode<Key>* nodeToDelete = NULL;
+  //  
+  //  if (nRet == 0){
+  //    nodeToDelete = node;
+  //    return nodeToDelete;
+  //  }else if (nRet < 0){
+  //    nodeToDelete = remove (node->_pLeft, key);
+  //  }else {
+  //    nodeToDelete = remove (node->_pRight, key);
+  //  }
+
+  //  if (nodeToDelete){
+  //    int nChildCase = 0;
+  //    if (nodeToDelete->_pLeft){
+  //      nChildCase = nChildCase | 1;
+  //    }
+
+  //    if (nodeToDelete->_pRight){
+  //      nChildCase = nChildCase | 2;
+  //    }
+
+  //    switch (nChildCase) {
+  //      case 0:
+  //        {
+  //          if (node->_pLeft == nodeToDelete)
+  //            node->_pLeft = NULL;
+  //          else
+  //            node->_pRight = NULL;
+
+  //          delete nodeToDelete;
+  //          nodeToDelete = NULL;
+  //        }break;
+  //      case 1: // only left child
+  //        {
+  //          if (node->_pLeft == nodeToDelete)
+  //            node->_pLeft = nodeToDelete->_pLeft;
+  //          else
+  //            node->_pRight = nodeToDelete->_pLeft;
+
+  //          if (nodeToDelete == _pRoot){
+  //            _pRoot = nodeToDelete->_pLeft;
+  //          }
+
+  //          delete nodeToDelete;
+  //          nodeToDelete = NULL;
+
+  //        }break;
+  //      case 2:// only rt child
+  //        {
+  //          if (node->_pLeft == nodeToDelete)
+  //            node->_pLeft = nodeToDelete->_pRight;
+  //          else
+  //            node->_pRight = nodeToDelete->_pRight;
+
+  //          if (nodeToDelete == _pRoot){
+  //            _pRoot = nodeToDelete->_pRight;
+  //          }
+
+  //          delete nodeToDelete;
+  //          nodeToDelete = NULL;
+
+  //        }break;
+  //      case 3:// both left and right child
+  //        {
+  //          TreeNode<Key>* _pNextNode = inorder_successor(node,nodeToDelete->_key);
+  //          nodeToDelete->_key = _pNextNode->_key;
+  //          remove(nodeToDelete->_pRight,nodeToDelete->_key);
+  //          
+  //          //copy contents of _pNextNode into nodeToDelete
+  //          //delete _pNextNode
+  //        }break;
+  //    }
+  //  }
+
+  //  return nodeToDelete;
+  //}
+
+public:
+  BSTRecord next ( Key& key)
+  {
+    TreeNode<Key>* nextNode = inorder_successor(key);
+    BSTRecord retRecord;
+    if (nextNode != NULL){
+      retRecord._key = &nextNode->_key;
+    }
+    return retRecord;
+  }
+
+  void remove (Key& key)
+  {
+    remove(_pRoot,key);
+  }
+
+private:
+  TreeNode<Key>* floor (TreeNode<Key>* node, Key& key)
+  {    
+    if (node == NULL)
+      return NULL;
+
+    TreeNode<Key>* retNode = NULL;
+    int nRet = node->_key.compare(key);
+    if (nRet < 0) {
+      retNode = floor(node->_pRight,key);
+      if (retNode == NULL){
+        retNode = node;
+      }
+    }else if (nRet > 0){
+      retNode = floor(node->_pLeft,key);
+    }else {
+      retNode = node;
+    }
+    return retNode;
+  }
+
+  TreeNode<Key>* ceil (TreeNode<Key>* node, Key& key)
+  {    
+    if (node == NULL)
+      return NULL;
+
+    TreeNode<Key>* retNode = NULL;
+    int nRet = node->_key.compare(key);
+    if (nRet < 0) {
+      retNode = ceil(node->_pRight,key);      
+    }else if (nRet > 0){
+      retNode = ceil(node->_pLeft,key);
+      if (retNode == NULL){
+        retNode = node;
+      }
+    }else {
+      retNode = node;
+    }
+
+    return retNode;
+  }
+public:
+  BSTRecord floor (Key& key)
+  {
+    TreeNode<Key>* _pFloorTreeNode = floor(_pRoot,key);    
+    BSTRecord record;
+    if (_pFloorTreeNode != NULL){
+      record._key = &_pFloorTreeNode->_key;
+    }   
+    return record;
+  }
+  BSTRecord ceil (Key& key)
+  {
+    TreeNode<Key>* _pCeilTreeNode = floor(_pRoot,key);    
+    BSTRecord record;
+    if (_pCeilTreeNode != NULL){
+      record._key = &_pCeilTreeNode->_key;
+    }  
+    return record;
+  }
+private:
+  void range (TreeNode<Key>* node, Key& startVal, Key& endVal, std::list<BSTRecord>& refList)
   {
     if (node == NULL)
-    int nRet = node->_key.compare(key);
-    if ()
-  }
-public:
-  //
-  Key& floor (Key& key)
-  {
-    TreeNode* _pFloorTreeNode = floor(_pRoot,key);    
+      return;
     
-  }
-  Key& ceil (Key& key)
-  {
+    int nRet1 = node->_key.compare(startVal);
+    int nRet2 = node->_key.compare(endVal);
 
+    if ( (nRet1 == 0) || (nRet2 == 0)){
+      BSTRecord record; record._key = &node->_key;
+      refList.push_back(record);
+    }else{
+      if ((nRet1 < 0) && (nRet2 > 0)){
+        
+        BSTRecord record; record._key = &node->_key;
+        refList.push_back(record);
+
+        range(node->_pLeft,startVal,endVal,refList);
+        range(node->_pRight,startVal,endVal,refList);
+      }
+
+      //if node.val < startVal then all elements in left subtree are invalid
+      // as left subtree < node.val < startVal
+      if (nRet1 > 0){
+        range(node->_pRight,startVal,endVal,refList);
+      }
+
+      if (nRet2 < 0){
+        range(node->_pLeft,startVal,endVal,refList);
+      } 
+    }
   }
 public:
-  void range (Key& startVal, Key& endVal)
+  std::list<BSTRecord> range (Key& startVal, Key& endVal)
   {
-
+    std::list<BSTRecord> resultList;
+    range(_pRoot,startVal,endVal,resultList);
+    return resultList;
   }
 private:
   TreeNode<Key>* _pRoot;
@@ -146,18 +363,41 @@ int _tmain(int argc, _TCHAR* argv[])
   bst.put(key1);
 
   
+  //while (1) {
+  //  std::cin >> key1.val;
+  //  BSTSymbolTable<IntKey>::BSTRecord record = bst.get(key1);
+  //  if (record.isValid()){
+  //    std::cout << key1.val << " Exists \n";
+  //  }else{
+  //    std::cout <<  key1.val << " Does not exist\n";
+  //  }
 
-  while (1) {
-    std::cin >> key1.val;
-    if (bst.exists(key1))
-      std::cout << key1.val << " Exists \n";
-    else
-      std::cout <<  key1.val << " Does not exist\n";
+  //  if (key1.val == -2)
+  //    break;
+  //}
 
-    if (key1.val == -2)
-      break;
+  key1.val = 2;
+  bst.remove(key1);
+  IntKey keyStart; keyStart.val = -1;
+  IntKey keyEnd; keyEnd.val = 4;
+
+  std::list<BSTSymbolTable<IntKey>::BSTRecord> rangeItems = bst.range(keyStart, keyEnd);
+  std::list<BSTSymbolTable<IntKey>::BSTRecord>::iterator iterList = rangeItems.begin();
+  while (iterList != rangeItems.end()){
+    std::cout << "Record: " << iterList->_key->val << std::endl;
+    iterList++;
   }
 
+
+  key1.val = -1;
+  BSTSymbolTable<IntKey>::BSTRecord record;
+  record._key = &key1;
+  do {
+    key1.val = record._key->val;
+    std::cout << key1.val << "--->";
+    record = bst.next(key1);
+  }while (record.isValid());
+  std::cout << "END" << std::endl;
 	return 0;
 }
 
